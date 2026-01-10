87f3607631dea31fa4ae1ef3ecfe8c01f44d0908
Simon Boeckner <115195101+SimonPBoeckner@users.noreply.github.com>
2026-01-10 15:34:43 -0500

    add .gitignore

--- src/pipeline/cameraPoseEstimator.cpp
+++ src/pipeline/cameraPoseEstimator.cpp
@@ -1,1 +1,13 @@
 #include "cameraPoseEstimator.hpp"
+
+CameraPoseEstimator::CameraPoseEstimator() {}
+
+CameraPoseEstimator::~CameraPoseEstimator() {}
+
+void CameraPoseEstimator::SolveCameraPose(zarray_t* detections) {
+    // if (field_layou == nullptr) return nothing
+
+    if (detections == nullptr || zarray_size(detections) == 0) {
+        return;
+    }
+}
